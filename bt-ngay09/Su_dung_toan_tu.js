18dfc5bf7576ee7a2546ca8d4d0c6b25ab665ef2
cuongdo90 <86915023+cuongdo90@users.noreply.github.com>
2021-07-11 18:59:29 +0700

    ok

---
+++ bt-ngay09/Su_dung_toan_tu.js
@@ -0,0 +1,4 @@
+let inputWidth = parseInt(prompt("Enter the width"));
+let inputHeight = parseInt(prompt("Enter the height"));
+let area = inputWidth * inputHeight;
+document.write("The area is: " + area);
